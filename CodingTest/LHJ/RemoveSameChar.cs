28cda2f5bbb7c7431acddbd4598841bb44080810
KiAcademia <kistudentmeta5@gmail.com>
2024-01-10 17:13:29 +0900

    re

--- CodingTest/LHJ/RemoveSameChar.cs
+++ CodingTest/LHJ/RemoveSameChar.cs
@@ -8,25 +8,20 @@ namespace CodingTest.LHJ
 {
     internal class RemoveSameChar
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            solution("people");
-        }
         static string solution(string my_string)
         {
-            string answer = "";
-            for (int i = 0; i < my_string.Length; i++)
+            string result = "";
+            HashSet<char> seen = new HashSet<char>();
+
+            foreach (char c in my_string)
             {
-                for (int j = i + 1; j < my_string.Length; j++)
+                if (!seen.Contains(c))
                 {
-                    if (my_string[i] == my_string[j])
-                        break;
-                    else if(j == my_string.Length - 1)
-                        answer += my_string[i];
+                    seen.Add(c);
+                    result+=c;
                 }
             }
-            return answer;
+            return result.ToString();
         }
     }
 }
--- CodingTest/Program.cs
+++ CodingTest/Program.cs
@@ -2,10 +2,10 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello, World!");
 
-        }
+        //}
     }
 }
