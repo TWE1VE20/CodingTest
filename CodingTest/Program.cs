98a03b0d06e667b2ab89201e4773d12c053fc61e
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-01-10 15:45:17 +0900

    feat : 평균 구하기

--- CodingTest/Program.cs
+++ CodingTest/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(Average.GetAverage(new int[] { 1, 2, 3 }));
         }
     }
 }
