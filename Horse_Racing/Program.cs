bc925e56a1d17e4364506ff711c9379eb5b6bea7
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-10 15:32:54 +0900

    feat : 달리기 경주 코테 solution 추가

---
+++ Horse_Racing/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Horse_Racing
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            static public string[] solution(string[] players, string[] callings)
+            {
+                string[] answer = new string[] { };
+
+                LinkedList<string> list = new LinkedList<string>();
+                var ranking = new Dictionary<string, LinkedListNode<string>>();
+
+                for (int i = 0; i < players.Length; i++)
+                    ranking.Add(players[i], list.AddLast(players[i]));
+
+                for (int j = 0; j < callings.Length; j++)
+                {
+                    LinkedListNode<string> buf = ranking[callings[j]].Previous;
+                    list.Remove(ranking[callings[j]].Previous);
+                    list.AddAfter(ranking[callings[j]], buf);
+                }
+
+                string[] buf2 = new string[players.Length];
+                list.CopyTo(buf2, 0);
+                answer = buf2;
+                return answer;
+            }
+
+            static void Main(string[] args)
+            {
+
+            }
+        }
+    }
+}
