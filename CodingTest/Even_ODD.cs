e2d84235e44e917416f0ca340b5341589d952cd1
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-10 16:48:35 +0900

    feat : 홀짝 구분하기 코드테스트 추가

---
+++ CodingTest/Even_ODD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    internal class Even_Odd
+    {
+        public void Even_odd()
+        {
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+            if (a % 2 == 0)
+                Console.WriteLine($"{a} is even");
+            else
+                Console.WriteLine($"{a} is odd");
+        }
+    }
+}
