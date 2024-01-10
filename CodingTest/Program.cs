92a46638da20e2aed380655215510953c2d83e9a
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-10 16:55:55 +0900

    feat : Program의 Main에 홀짝 구분하기 코드테스트 실행코드 추가

--- CodingTest/Program.cs
+++ CodingTest/Program.cs
@@ -11,6 +11,11 @@ namespace CodingTest
             string[] players = ["mumu", "soe", "poe", "kai", "mine"];
             string[] callings = ["kai", "kai", "mine", "mine"];
             horse_racing.ArrayWrite<string>(horse_racing.solution(players, callings));
+
+            //홀짝 구분하기 코드테스트 실행코드
+            //https://school.programmers.co.kr/learn/courses/30/lessons/181920 풀었다
+            Even_Odd eo = new Even_Odd();
+            eo.Even_odd();
         }
     }
 }
