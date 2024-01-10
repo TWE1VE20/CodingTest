ac1f2847859cebb4d2d01ce69cb04430b35e2de1
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-10 16:38:05 +0900

    docs : 달리기 경주 코드 테스트 링크 추가

--- CodingTest/Program.cs
+++ CodingTest/Program.cs
@@ -6,6 +6,7 @@ namespace CodingTest
         static void Main(string[] args)
         {
             //달리기 경주 코드테스트 실행코드
+            //https://school.programmers.co.kr/learn/courses/30/lessons/178871 풀었다
             Horse_Racing horse_racing = new Horse_Racing();
             string[] players = ["mumu", "soe", "poe", "kai", "mine"];
             string[] callings = ["kai", "kai", "mine", "mine"];
